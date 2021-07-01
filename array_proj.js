e4bfe02c54c0b824d8d2e7994c1bbd4cc423733b
Sergey <koleda.exceedteam@gmail.com>
2021-07-01 16:11:34 +0300

    several additional solutions

--- array_proj.js
+++ array_proj.js
@@ -2,6 +2,13 @@ let mas = ['cat', 'dog', 'parrot', 'horse', 'fish', 'chicken', 'lion'];
 let mas0=mas.slice('');
 let mas1=mas.slice('');
 let mas2=mas.slice('');
+let mas3=mas.slice('');
+let mas01=mas.slice('');
+let mas4=mas.slice('');
+let mas001=mas.slice('');
+let mas5=mas.slice('');
+
+
 
 //удаление fish
 for (let i=0; i < mas.length; i++ )
@@ -22,11 +29,35 @@ let c=0;
 while( c !=2){
     mas1.pop();
     c++;}
-
+//----//    
+mas01.length=mas01.length-2;
+//-----//
+mas001.splice(-2,2);
 //удаление первого элемента (cat)
     mas2.shift()
 
-console.log(mas);
-console.log(mas0);
-console.log(mas1);   
-console.log(mas2);
+//ещё способы удалить элемент(fish)
+mas3.forEach((mass,index)=>{
+    if (mas3[index]=='fish')
+            mas3.splice(index,1);
+})
+//---------//
+let index1=mas4.indexOf('fish',0);
+mas4.splice(index1,1);
+//----//
+mas5.map((mass,index2)=>{   
+    if (mas5[index2]=='fish')         
+        mas5.splice(index2,1); 
+})
+
+
+
+console.log(1,mas);
+console.log(2,mas0);
+console.log(3,mas1);   
+console.log(4,mas01);
+console.log(5,mas001);
+console.log(6,mas2);
+console.log(7,mas3);
+console.log(8,mas4);
+console.log(9,mas5);
